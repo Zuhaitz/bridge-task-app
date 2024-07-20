b52a705027fef2de24fc05d572f2429a8dd8a2f5
Zuhaitz <zuhaitzmartinezjimenez@gmail.com>
2024-07-20 13:54:31 +0200

    Search bar half done

--- src/components/search-bar/SearchBar.jsx
+++ src/components/search-bar/SearchBar.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import "./SearchBar.scss";
+
+import plusIcon from "../../assets/icons/plus.svg";
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  return (
+    <div className="search">
+      <input className="search__input" placeholder="Add a new task" />
+      <button className="search__btn">
+        <img src={plusIcon} alt="Add button" />
+      </button>
+    </div>
+  );
 };
 
 export default SearchBar;
